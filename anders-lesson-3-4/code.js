566516e1f053c7d969df6ffdcfbae9285e1ef94b
Bennybre <sjolund_micke@msn.com>
2026-01-12 14:02:37 +0100

    dice roll, multiple dices, yatzy like roll.

--- anders-lesson-3-4/code.js
+++ anders-lesson-3-4/code.js
@@ -13,6 +13,8 @@ const n4Code = n4.querySelector("code");
 
 const n5 = document.querySelector("#n5");
 const n5Code = n5.querySelector("code");
+const multiple = n5.querySelector("div#multiple");
+const yatzy = n5.querySelector("div#yatzy");
 
 // console.log("n1", n1);
 // console.log("n1Code", n1Code);
@@ -129,9 +131,95 @@ flipCoinV().then((result) => {
 
 // Kasta tärning när man klickar på något.
 
+// 1. Lägg till en bild på en tärning.
+// 2. Se till att man kan klicka på bilden.
+// 3. När man klicka på tärningen ska ett eller flera tärningskast simuileras.
+// 4. Resultatet av tärningskast ska presentteras i elemenet <code></code>
+
+// Lösning 1
+// Använd det inbyggda objektet Image() för att skapa en bild.
+// Tilldela elemet ett src attribut
+// Med html: <img src="https://upload.wikimedia.org/wikipedia/commons/c/c4/2-Dice-Icon.svg" alt="ett par tärningar.">
+
+// Med Javscript
+// Ny Bild
+let img = new Image();
+
+// Använd attributet src
+img.src = "https://upload.wikimedia.org/wikipedia/commons/c/c4/2-Dice-Icon.svg";
+
+// Ange attributet alt
+img.alt = "ett par träningar"
+
+n5.appendChild(img);
+
+for (let i = 1; i <= 5; i++) {
+
+    // kastas tärning..
+    rollDice().then((result) => {
+        // multiple.textContent += result + "\n";
+
+        // Skapa ett nytt element för varje tärningskast
+        // Visa vilken tärning som det gäller
+        const div = document.createElement("div");
+        div.textContent = `Tärning ${i} visar ${result}`;
+        div.classList = "dice result";
+        multiple.appendChild(div);
+    });
+}
+
+// Kasta 5 tärningar och inväta resultat då samtliga 5 tärningar stannat.
+// I Promise objektet kan man inväta mer än ett utfall - Promise.all
+
+// En array för tärningar
+const dices = [];
+
+// 5 tärningskast
+for (let i = 0; i < 5; i++) {
+    const dice = rollDice();
+    dices.push(dice);
+}
+
+Promise.all(dices).then((values) => {
+    yatzy.textContent = values.join(",");
+});
+
+
+
 
 // Händelselyssnare
 
+img.addEventListener("click", (e) => {
+    console.log(`Ett klick på elementet ${e.target}`);
+
+    // Kastas en tärning
+    rollDice().then((result) => {
+        n5Code.textContent = result;
+    });
+
+});
 
 
 // Funktioner
+
+function rollDice() {
+    return new Promise((resolve) => {
+
+
+        // Låt de ta lite tid
+        setTimeout(() => {
+
+            // Ett resultat mellan ca 0.000006 och 5.99999994
+            const randomFloat = Math.random() * 6;
+
+            // // Funktion flr att avrunda ett tal till närmsta större heltal.
+            // Math.ceil();
+
+            // // Funktion för att avrunda ett tal till närmsta mindre heltal.
+            // Math.floor();
+
+            resolve(Math.ceil(randomFloat));
+
+        }, Math.random() * 3000 );
+    });
+}
